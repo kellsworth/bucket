5f34a07462833025d977ce2c4958566020ac4329
Karen - iMac <kellsworth628@gmail.com>
2021-04-30 07:12:34 -0600

    update

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -8,9 +8,7 @@ class App extends React.Component {
     this.state = {
       name: '',
       email: '',
-      title: '',
-      message: '',
-      image:''
+      message: ''
     }
   }
 
@@ -21,13 +19,11 @@ class App extends React.Component {
 
   handleSend = () => {
     const { name, email, message, title ,image} = this.state
-    axios.post('/api/email', { name, email, message, title,image }).then(res => {
+    axios.post('/api/email', { name, email, message }).then(res => {
       this.setState({
         name: '',
         email: '',
-        title: '',
-        message: '',
-        image:''
+        message: ''
       })
     })
   }
@@ -39,11 +35,9 @@ class App extends React.Component {
         <section className="hero">
           <div style={styles.form}>
             <h1 style={styles.header}>Email Us</h1>
-          <input style={styles.input} placeholder='title' type="text" name='title' value={title} onChange={this.handleInput} />
           <input style={styles.input} placeholder='name' type="text" name='name' value={name} onChange={this.handleInput} />
           <input style={styles.input} placeholder='email' type="text" name='email' value={email} onChange={this.handleInput} />
           <input style={styles.input} placeholder='message' type="text" name='message' value={message} onChange={this.handleInput} />
-          <input style={styles.input} placeholder='image' type="text" name='image' value={image} onChange={this.handleInput} />
           <button style={styles.button} onClick={this.handleSend}>Send</button>
         </div>
         </section>
